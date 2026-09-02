fecda5aa7c11f32efffac6c21cdc7dfe989fa0fe
tskwon <pumpjun@gmail.com>
2026-09-02 11:08:05 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
 
-st.set_page_config(layout="wide", initial_sidebar_state="expanded", page_title="T/S Smart Match", page_icon="logo.png")
+st.set_page_config(layout="wide", initial_sidebar_state="expanded", page_title="Spectra50 Smart Match", page_icon="logo.png")
 
 # ==========================================
 # 구글 시트 연결 및 조제표 자동 매칭 함수
@@ -200,7 +200,7 @@ st.markdown(f"""
 </style>
 <div class="fixed-header">
     <img src="data:image/png;base64,{logo_base64}" onerror="this.style.display='none'">
-    <h2>T/S Smart Match</h2>
+    <h2>Spectra50 Smart Match</h2>
 </div>
 """, unsafe_allow_html=True)
 
